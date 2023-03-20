8c48ca16bd2e54a1e93ceaeb233240766baaa96c
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-03-20 08:28:13 -0300

    Clase 01 - Insercion de JavaScript

---
+++ Clase_01/app.js
@@ -0,0 +1,17 @@
+alert('Hola desde JavaScript');
+// Muestra un mensaje de confirmación
+confirm('¿Seguro que queres Salir?'); 
+
+/*
+Esto es otro comentario pero
+desde JavaScript
+*/
+
+// Solicitar un dato por teclado del usuario
+
+prompt('¿Cúal es tu nombre?');
+
+console.log('Hola desde la consola');
+
+// Mostrar un texto en el documento
+document.write('Soy un texto');
